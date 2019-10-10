c2051045615a669dc948cb3264eead2136206ea6
Levminer <33373714+Levminer@users.noreply.github.com>
2019-10-10 15:51:29 +0200

    .

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
 # Autómatikus Leállító Program
 
-* Egy nagyon egyszerű CMD alapú program ami a megfelelő gomb lenyomása után leállítója a számítógépet egy megadott idő után.
+* Egy nagyon egyszerű parancssor alapú program ami a megfelelő gomb lenyomása után leállítja a számítógépet egy megadott idő után.
 
-# Fukciók
+## Fukciók
 
 * Leállítás 15 perc, 30 perc, 45 perc, 60 perc után.
 * Leállítás saját érték megadása után.
+* Leállítás az előző megadott érték után
 * Betüszín beállítása.
 
-# Letöltés
+## Letöltés
 
 * [V1 Letöltés](https://github.com/Levminer/ALP-ASP/releases/tag/V1)
 * [V2 Letöltés](https://github.com/Levminer/ALP-ASP/releases/tag/V2)
+
+# Automatic Shutdown Program
+
+* A very simple command line program that turns off the computer in a given time.
+
+## Functions
+
+* Shutdown in 15, 30, 45, 60, minutes.
+* Shutdown by costum value.
+
+## Download
+
+* [V1 Download](https://github.com/Levminer/ALP-ASP/releases/tag/V1)
--- SOURCECODE/V2/ALP V2 HU.bat
+++ SOURCECODE/V2/ALP V2 HU.bat
@@ -1,14 +1,15 @@
+
+//Aut¢matikus Le llit¢ Program V2
+//K‚sz¡tette: Levminer
+
 @echo off
 title Aut¢matikus Le llit¢ Program V2 - K‚sz¡tette: Levminer
 cd C:\Users\%USERNAME%\AppData\Local\Temp\ALP\
 
-//memory
+//color
 set file=color.txt
-for /f "tokens=*" %%A in (%file%) do (%%A)
-//memory
-
-//Aut¢matikus Le llit¢ Program V2
-//K‚sz¡tette: Levminer
+for /f "tokens=*" %%C in (%file%) do (%%C)
+//color
 
 //start
 :start
@@ -17,12 +18,13 @@ echo.
 echo Aut¢matikus Le llit¢ Program V2
 echo K‚sz¡tette: Levminer
 echo.
-echo (0) Le ll¡t s visszavon sa
-echo (1) Negyed ¢ra m£lva
-echo (2) F‚l ¢ra m£lva
-echo (3) H romnegyed ¢ra m£lva
-echo (4) Egy ¢ra m£lva
-echo (5) Saj t ‚rtek 
+echo (0) Le ll¡t s visszavon sa 
+echo (1) Le ll¡t s 15 perc ut n
+echo (2) Le ll¡t s 30 perc ut n
+echo (3) Le ll¡t s 45 perc ut n
+echo (4) Le ll¡t s 60 perc ut n
+echo (5) Le ll¡t s egy megadott ‚rt‚k ut n
+echo (6) Le ll¡t s az el‹z‹ megadott ‚rt‚k ut n
 echo (8) Be ll¡t sok
 echo (9) Kil‚p‚s
 echo.
@@ -33,6 +35,7 @@ if %menu%==2 goto 2
 if %menu%==3 goto 3 
 if %menu%==4 goto 4
 if %menu%==5 goto 5
+if %menu%==6 goto 6
 if %menu%==8 goto 8
 if %menu%==9 goto 9
 goto start
@@ -52,9 +55,9 @@ cls
 
 :1
 cls
-shutdown -s -t 900 -c "Le ll¡tas negyed ¢ra m£lva!"
+shutdown -s -t 900 -c "Le ll¡t s 15 perc m£lva!"
 echo.
-echo Le ll¡tas negyed ¢ra m£lva!
+echo Le ll¡t s 15 perc m£lva!
 echo.
 pause
 goto start
@@ -62,9 +65,9 @@ cls
 
 :2
 cls
-shutdown -s -t 1800 -c "Le ll¡tas f‚l ¢ra m£lva!"
+shutdown -s -t 1800 -c "Le ll¡t s 30 perc m£lva!"
 echo.
-echo Le ll¡tas f‚l ¢ra m£lva!
+echo Le ll¡t s 30 perc m£lva!
 echo.
 pause
 goto start
@@ -72,9 +75,9 @@ cls
 
 :3
 cls
-shutdown -s -t 2700 -c "Le ll¡tas h romnegyed ¢ra m£lva!"
+shutdown -s -t 2700 -c "Le ll¡t s 45 perc m£lva!"
 echo.
-echo Le ll¡tas h romnegyed ¢ra m£lva!
+echo Le ll¡t s 45 perc m£lva!
 echo.
 pause
 goto start
@@ -82,22 +85,22 @@ cls
 
 :4
 cls
-shutdown -s -t 3600 -c "Le ll¡tas egy ¢ra m£lva!"
+shutdown -s -t 3600 -c "Le ll¡t s 60 perc m£lva!"
 echo.
-echo Le ll¡tas egy ¢ra m£lva!
+echo Le ll¡t s 60 perc m£lva!
 echo.
 pause
 goto start
 cls
 //ertekek
 
-//sajatertek
+//megadottertek
 :5
 cls
 echo.
 set /p m="Add meg az ‚rteket percben! % %"
 set /a m1=%m%
-set /a s= %m% * 60
+set /a s=%m% * 60
 cls
 
 :5.1
@@ -107,9 +110,31 @@ echo.
 echo Le ll¡tas %m1% perc m£lva!
 echo.
 pause
-goto start
 cls
-//sajatertek
+
+:5.2
+echo %m1% > value.txt
+goto start 
+//megadottertek
+
+//elozomegadottertek
+:6
+set file=value.txt
+for /f "Delims=" %%V in (%file%) do (set mm=%%V)
+
+:6.1
+set /a mm1=%mm%
+set /a ss=%mm% * 60
+
+:6.2
+cls
+shutdown -s -t %ss% -c "Le ll¡tas %mm1% perc m£lva!"
+echo.
+echo Le ll¡tas %mm1% perc m£lva!
+echo.
+pause
+goto start
+//elozomegadottertek
 
 //beallitasok
 :8
@@ -120,29 +145,27 @@ echo (2) Feh‚r
 echo (3) Z”ld
 echo.
 set /p color="V lassz sz¡nt! (1-3) % %"
-if %color%==1 goto color1
-if %color%==2 goto color2
-if %color%==3 goto color3
-goto :start
+if %color%==1 goto 8.1
+if %color%==2 goto 8.2
+if %color%==3 goto 8.3
+goto start
 cls
-//beallitasok
 
-//colors
-:color1
+:8.1
 echo color 4 > color.txt
 color 4
 goto start
 
-:color2
+:8.2
 echo color F > color.txt
 color F
 goto start
 
-:color3
+:8.3
 echo color 2 > color.txt
 color 2
 goto start
-//colors
+//beallitasok
 
 
 //kilepes
